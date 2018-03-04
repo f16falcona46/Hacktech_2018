78ebe1b7212123da13f71fbd3816e52a456805a9
Jason Li <li07.jason@gmail.com>
2018-03-04 06:52:01 -0800

    Integrated GPS. Still needs rate limiting.

--- Backend/DesktopServer.tcl
+++ Backend/DesktopServer.tcl
@@ -22,6 +22,25 @@ proc geturl_followRedirects {url args} {
 	}
 }
 
+proc deg2rad {deg} {
+  return [expr {$deg * (3.1415926535897931/180)}]
+}
+
+proc sphericalDistance {lat1 lon1 lat2 lon2} {
+	#Radius of the earth in km
+	set R 6371
+	set dLat [deg2rad [expr {$lat2 - $lat1}]]
+	set dLon [deg2rad [expr {$lon2 - $lon1}]]
+	set lat1 [deg2rad $lat1]
+	set lon1 [deg2rad $lon1]
+	set lat2 [deg2rad $lat2]
+	set lon2 [deg2rad $lon2]
+	set a [expr {sin($dLat / 2) * sin($dLat / 2) + cos($lat1) * cos($lat2) * sin($dLon / 2) * sin($dLon / 2)}]
+	set c [expr {2 * atan2(sqrt($a), sqrt(1 - $a))}] 
+	set d [expr {$R * $c}]
+	return $d
+}
+
 set curdir [file dirname [info script]]
 
 source [file join $curdir "TemplaTcl.tcl"]
@@ -34,6 +53,14 @@ verify_step2Template parseFile [file join $curdir "verify_step2.htmlt"]
 TemplaTcl::create verify_step3Template
 verify_step3Template parseFile [file join $curdir "verify_step3.htmlt"]
 
+set door_loc_fd [open [file join $curdir "door.loc"] r]
+set door_loc [read $door_loc_fd]
+close $door_loc_fd
+
+
+set latitude 0
+set longitude 0
+
 sqlite3 verifyDb [file join $curdir "verification.db"]
 
 fcgi Init
@@ -41,8 +68,6 @@ set sock [fcgi OpenSocket :8000]
 set req [fcgi InitRequest $sock 0]
 
 while {1} {
-	puts "Got a request."
-	
 	fcgi Accept_r $req
 	#get the requested page
 	set pd [fcgi GetParam $req]
@@ -100,7 +125,20 @@ while {1} {
 		} elseif {[dict get $query_params "action"] eq "updategps"} {
 			set latitude [dict get $query_params "lat"]
 			set longitude [dict get $query_params "long"]
-			puts "Latitude: $latitude, Longitude: $longitude"
+			set distance [sphericalDistance [lindex $door_loc 0] [lindex $door_loc 1] $latitude $longitude]
+			puts "Latitude: $latitude, Longitude: $longitude, Distance: $distance"
+			if {$distance < 0.02} {
+				::http::data [geturl_followRedirects "http://localhost:8062/check"]
+			}
+		} elseif {[dict get $query_params "action"] eq "calibrate_door"} {
+			set door_loc ""
+			lappend door_loc $latitude
+			lappend door_loc $longitude
+			set door_loc_fd [open [file join $curdir "door.loc"] w]
+			puts $door_loc_fd $door_loc
+			close $door_loc_fd
+			verify_step3Template setVar SUCCESS "Your last GPS location was set as the door location."
+			append C [verify_step3Template render]
 		}
 	} else {
 		append C "null home page"
