cb804009ebddab6569d7197c11852adb0c855dc5
Jason Li <li07.jason@gmail.com>
2018-03-03 19:20:32 -0800

    Finished removing old stuff

--- PrepPWM.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo 0 > /sys/class/pwm/pwmchip0/unexport
-echo 0 > /sys/class/pwm/pwmchip0/export
-sudo chown pi /sys/class/pwm/pwmchip0/pwm0/*
-echo 20000000 > /sys/class/pwm/pwmchip0/pwm0/period
-echo 800000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
-echo 1 > /sys/class/pwm/pwmchip0/pwm0/enable
