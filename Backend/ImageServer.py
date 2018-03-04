78a5ec6d5ae13e90962e79ac1d6dcf9debd7995e
Jason Li <li07.jason@gmail.com>
2018-03-04 07:22:03 -0800

    Added interaction with Dragonboard (Pi side only for now)

--- Backend/ImageServer.py
+++ Backend/ImageServer.py
@@ -4,18 +4,25 @@ import wsgiref.simple_server
 import cgi
 import base64
 import subprocess
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+GPIO.output(4, 1)
 
 def app(env, start_response):
 	if env["PATH_INFO"] == "/lock":
 		start_response("200 OK", [("Content-type", "text/html; charset=utf-8")])
 		with open('/sys/class/pwm/pwmchip0/pwm0/duty_cycle', 'w') as pwm:
-			pwm.write("800000\n")
+			pwm.write("800000\n")	
+		GPIO.output(4, 1)
 		response = [b"""Locked."""]
 		return response
 	elif env["PATH_INFO"] == "/unlock":
 		start_response("200 OK", [("Content-type", "text/html; charset=utf-8")])
 		with open('/sys/class/pwm/pwmchip0/pwm0/duty_cycle', 'w') as pwm:
 			pwm.write("2400000\n")
+		GPIO.output(4, 0)
 		response = [b"""Unlocked."""]
 		return response
 	elif env["PATH_INFO"] == "/picture.jpg":
@@ -31,6 +38,7 @@ def app(env, start_response):
 		response.append(b"""</table><hr /><a href="/">Home</a></html>""")
 		return response
 	else:
+		start_response("200 OK", [("Content-type", "text/html; charset=utf-8")])
 		return [b"""Unsupported<hr /><a href="/">Home</a>"""]
 
 httpd = wsgiref.simple_server.make_server("raspberrypi.local", 8051, app)
